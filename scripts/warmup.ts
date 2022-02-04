b004841048457f6bf7e03d626677e3c005a62b83
Sergey Zabelin <ns6630@gmail.com>
2022-02-04 20:07:26 +0300

    Удален вывод ошибок в основной поток пока контейнер с MariaDB не готов.

--- scripts/warmup.ts
+++ scripts/warmup.ts
@@ -1,11 +1,16 @@
-import { execSync } from "child_process";
+import * as util from "util";
+import chaild_process from "child_process";
+const exec = util.promisify(chaild_process.exec);
 
-function warmup() {
+async function warmup() {
   const command = `docker exec mariadb-tests /bin/bash -c "mysqladmin ping --password=$MARIADB_ROOT_PASSWORD"`;
   try {
-    const output = execSync(command).toString();
+    const { stdout } = await exec(command);
+    const output = stdout.toString();
     if (!output.includes("mysqld is alive")) {
       setTimeout(warmup, 1000);
+    } else {
+      console.log("MariaDB is ready.");
     }
   } catch (e) {
     setTimeout(warmup, 1000);
